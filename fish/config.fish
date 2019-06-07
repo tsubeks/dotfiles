006f476e7657efc523f2381e4bc0f03416af73b4
Bogdan Tsubeks <tsubekx@gmail.com>
2019-06-07 15:34:35 +0300

    comment FZF var

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 # Exports
 set -x TERM "xterm-256color"
 set -x EDITOR nvim
-set -x FZF_DEFAULT_COMMAND "fd --type f --hidden --follow --exclude .git"
+# set -x FZF_DEFAULT_COMMAND "fd --type f --hidden --follow --exclude .git"
 set -x PATH "$PATH:$HOME/Code/flutter/bin"
 
 
