3c29bcf5ddb250336d2c75cdfbd6aad537dd7ac2
Bogdan Tsubeks <tsubekx@gmail.com>
2019-06-07 15:52:56 +0300

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,13 @@
 # Exports
 set -x TERM "xterm-256color"
 set -x EDITOR nvim
-# set -x FZF_DEFAULT_COMMAND "fd --type f --hidden --follow --exclude .git"
 set -x PATH "$PATH:$HOME/Code/flutter/bin"
+# set -x FZF_DEFAULT_COMMAND "fd --type f --hidden --follow --exclude .git"
 
 
-alias vim="nvim"
-alias tmux="tmux -f ~/.config/tmux/tmux.conf"
-alias ls="exa -l"
-alias code="code-insiders"
-alias ci="code-insiders"
+alias vim "nvim"
+alias tmux "tmux -f ~/.config/tmux/tmux.conf"
+alias ls "exa -l"
+alias edit "code-insiders"
+alias code "code-insiders"
+alias ci "code-insiders"
